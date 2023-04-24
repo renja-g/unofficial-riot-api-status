c0b64a388e67ef369070ec97d2c950d1194ca89b
Renja <renja.grotemeyer@gmail.com>
2023-04-25 00:30:35 +0200

    Rebuild config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,50 +1,66 @@
-interface Region {
-  name: string;
-  url: string;
+interface Endpoint {
+	name: string;
+	regions: string[];
+	children: Children[];
 }
 
-interface Endpoint {
-  name: string;
-  url: string;
-  regions: Region[];
+interface Children {
+	children_url: string;
 }
 
 interface Config {
-  endpoints: Endpoint[];
+	baseUrl: string;
+	endpoints: Endpoint[];
 }
 
-
 const config: Config = {
-  "endpoints": [
-    {
-      "name": "SUMMONER-V4",
-      "url": "https://developer.riotgames.com/apis#summoner-v4",
-      "regions": [
-        {
-          "name": "EUW1",
-          "url": "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/someone"
-        },
-        {
-          "name": "NA1",
-          "url": "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/someone"
-        }
-      ]
-    },
-    {
-      "name": "LEAGUE-V4",
-      "url": "https://developer.riotgames.com/apis#league-v4",
-      "regions": [
-        {
-          "name": "EUW1",
-          "url": "https://euw1.api.riotgames.com/lol/league/v4/grandmasterleagues/by-queue/RANKED_SOLO_5x5"
-        },
-        {
-          "name": "NA1",
-          "url": "https://na1.api.riotgames.com/lol/league/v4/grandmasterleagues/by-queue/RANKED_SOLO_5x5"
-        },
-      ],
-    },
-  ],
+	baseUrl: 'https://{region}.api.riotgames.com',
+	endpoints: [
+		{
+			name: 'ACCOUNT-V1',
+			regions: ['americas', 'asia', 'esports', 'europe'],
+			children: [
+				{
+					children_url: '/riot/account/v1/accounts/by-puuid/{puuid}'
+				},
+				{
+					children_url: '/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}'
+				},
+				//      {
+				//        'url': '/riot/account/v1/accounts/me',
+				//      },
+				{
+					children_url: '/riot/account/v1/active-shards/by-game/{game}/by-puuid/{puuid}'
+				}
+			]
+		},
+		{
+			name: 'LEAGUE-V4 ',
+			regions: ['br1', 'eun1', 'euw1', 'jp1', 'kr', 'la1', 'la2', 'na1', 'oc1', 'ph2', 'ru', 'sg2', 'th2', 'tr1', 'tw2', 'vn2'],
+			children: [
+				{
+					children_url: '/lol/league/v4/challengerleagues/by-queue/{queue}'
+				},
+				{
+					children_url: '/lol/league/v4/entries/by-summoner/{encryptedSummonerId}'
+				},
+				{
+					children_url: '/lol/league/v4/entries/{queue}/{tier}/{division}'
+				},
+				{
+					children_url: '/lol/league/v4/grandmasterleagues/by-queue/{queue}'
+				},
+				{
+					children_url: '/lol/league/v4/leagues/{leagueId}'
+				},
+				{
+					children_url: '/lol/league/v4/masterleagues/by-queue/{queue}'
+				}
+			]
+		}
+	]
 };
 
 export default config;
+
+// full_url = https://{region}.api.riotgames.com{children_url}
