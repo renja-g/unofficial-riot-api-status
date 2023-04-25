aaa2e31e66db3ef3547071b422de74833475203c
renja-grotemeyer <renja.grotemeyer@gmail.com>
2023-04-25 08:49:11 +0200

    Added CHAMPION-V3 to config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -53,6 +53,15 @@ const config: Config = {
 			]
 		},
 		{
+			name: 'CHAMPION-V3',
+			regions: ['br1', 'eun1', 'euw1', 'jp1', 'kr', 'la1', 'la2', 'na1', 'oc1', 'ph2', 'ru', 'sg2', 'th2', 'tr1', 'tw2', 'vn2'],
+			children: [
+				{
+					children_url: '/lol/platform/v3/champion-rotations'
+				}
+			]
+		},
+		{
 			name: 'LEAGUE-V4 ',
 			regions: ['br1', 'eun1', 'euw1', 'jp1', 'kr', 'la1', 'la2', 'na1', 'oc1', 'ph2', 'ru', 'sg2', 'th2', 'tr1', 'tw2', 'vn2'],
 			children: [
