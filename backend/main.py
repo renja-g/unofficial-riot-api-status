e23056c8c79f306217ea8d9d171a8d86a03522e8
Renja <renja.grotemeyer@gmail.com>
2023-04-29 12:44:38 +0200

    something

--- backend/main.py
+++
@@ -1,6 +0,0 @@
-import requests
-import json
-
-with open('endpoints.json', 'r') as f:
-    endpoints_data = json.load(f)
-
---
+++ backend/setup.py
@@ -0,0 +1,6 @@
+import requests
+import json
+
+"""
+Autpomatically configures the endpoints.json with the correct API KEY
+"""
