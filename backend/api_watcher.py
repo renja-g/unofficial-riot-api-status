d6f877335259d019b2efa798c09b4bfb6171d29d
Renja <renja.grotemeyer@gmail.com>
2023-05-04 23:02:48 +0200

    v1 of an working output

--- backend/api_watcher.py
+++ backend/api_watcher.py
@@ -3,6 +3,10 @@ import os
 import asyncio
 import aiohttp
 from dotenv import load_dotenv
+from rich.console import Console
+from rich.table import Table
+
+
 
 load_dotenv() # load variables from .env file
 
@@ -54,39 +58,51 @@ async def make_request(url: str, api_key: str, session: aiohttp.ClientSession) -
                 return {'data': data, 'status': response.status}
 
 
-async def get_all_urls():
-    urls = []
+async def get_grouped_urls():
+    urls = {}
     for game in endpoints_data['endpoints']:
         game_endpoints = endpoints_data['endpoints'][game]
         for endpoint in game_endpoints:
+            endpoint_name = endpoint['name']
             regions = endpoint['regions']
             for region in regions:
                 for child in endpoint['children']:
                     child_url = child['child_url']
                     url = await build_url(base_url=endpoints_data['baseUrl'], game=game, endpoint=child_url, region=region)
-                    urls.append(url)
+                    if endpoint_name not in urls:
+                        urls[endpoint_name] = {}
+                    if region not in urls[endpoint_name]:
+                        urls[endpoint_name][region] = []
+                    urls[endpoint_name][region].append(url)
     return urls
 
-
-async def fetch_all(urls, api_key, session):
+async def get_responses(urls: list, api_key: str, session: aiohttp.ClientSession) -> list:
     tasks = []
     for url in urls:
-        task = asyncio.ensure_future(make_request(url=url, api_key=api_key, session=session))
-        tasks.append(task)
+        tasks.append(make_request(url, api_key, session))
     responses = await asyncio.gather(*tasks)
     return responses
 
-
 async def main():
-    urls = await get_all_urls()
-    async with aiohttp.ClientSession() as session:
-        responses = await fetch_all(urls, api_key=API_KEY, session=session)
-        for response, url in zip(responses, urls):
-            print(f'{response["status"]} - {url}')
+    console = Console()
+    urls = await get_grouped_urls()
 
-    # Save to json file
-    with open('responses.json', 'w') as f:
-        json.dump(responses, f, indent=4)
+    async with aiohttp.ClientSession() as session:
+        # Make requests to all URLs and print the status codes.
+        for endpoint in urls:
+            print(endpoint)
+            for region in urls[endpoint]:
+                urls_to_request = urls[endpoint][region]
+                responses = await get_responses(urls_to_request, API_KEY, session)
+                table = Table(title=f"{endpoint} ({region})")
+                table.add_column("URL")
+                table.add_column("Status", justify="right")
+                for response, url in zip(responses, urls_to_request):
+                    status = str(response['status'])
+                    status_color = "green" if status == "200" else "red"
+                    table.add_row(url, f"[{status_color}]{status}[/{status_color}]")
+                console.print(table)
+            print()
 
 
 if __name__ == '__main__':
